d8be69839b0fa71726f6a245597b66c9db5c3797
Sofia Tejada <sofianais@icloud.com>
2021-07-19 19:27:23 -0700

    write test that checks if capitalizeAndFilter does what it says it does

---
+++ capitalize-and-filter.js
@@ -0,0 +1,5 @@
+const capitalizeAndFilter = () => {
+  return ['Fearow', 'Fomantis'];
+};
+
+export default capitalizeAndFilter;
---
+++ capitalize-and-filter.test.js
@@ -0,0 +1,11 @@
+import capitalizeAndFilter from './capitalize-and-filter';
+
+describe('capitalizeAndFilter', () => {
+  it('takes an array of strings, capitalizes it, and filters out any string that starts with the letter F/f', () => {
+    const someArray = ['pikachu', 'fearow', 'solgaleo', 'necrozma', 'fomantis'];
+    const expected = ['Fearow', 'Fomantis'];
+    const actual = capitalizeAndFilter(someArray);
+
+    expect(actual).toEqual(expected);
+  });
+});
