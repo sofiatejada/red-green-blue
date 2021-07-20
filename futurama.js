d51294623437707fea92c11c6cc8a88671f62c67
Sofia Tejada <sofianais@icloud.com>
2021-07-20 10:42:31 -0700

    formatted what i want the returned object to look like

--- futurama.js
+++ futurama.js
@@ -3,9 +3,12 @@ import fetch from 'node-fetch';
 const fetchQuotes = async () => {
   const data = await fetch('http://futuramaapi.herokuapp.com/api/quotes');
   const body = await data.json();
-  
-  console.log(body);
-  return body;
+  const quote = {
+    name: body[0].character,
+    text: body[0].quote,
+    image: body[0].image,
+  };
+  console.log(quote);
 };
 
 export default fetchQuotes;
