a9bc700412eac57f6330d7051b4a4ba781ac243e
Sofia Tejada <sofianais@icloud.com>
2021-07-19 19:50:44 -0700

    well...something happens? tests pass, fam

--- capitalize-and-filter.js
+++ capitalize-and-filter.js
@@ -1,5 +1,13 @@
-const capitalizeAndFilter = () => {
-  return ['Fearow', 'Fomantis'];
+const capitalizeAndFilter = (arr) => {
+  const accum = [];
+  for(const item of arr) {
+    if(item[0] === 'f') {
+      const capitalized = item.toUpperCase();
+      accum.push(capitalized);
+      console.log(accum);
+    }
+  }
+  return accum;
 };
 
 export default capitalizeAndFilter;
--- capitalize-and-filter.test.js
+++ capitalize-and-filter.test.js
@@ -3,7 +3,7 @@ import capitalizeAndFilter from './capitalize-and-filter';
 describe('capitalizeAndFilter', () => {
   it('takes an array of strings, capitalizes it, and filters out any string that starts with the letter F/f', () => {
     const someArray = ['pikachu', 'fearow', 'solgaleo', 'necrozma', 'fomantis'];
-    const expected = ['Fearow', 'Fomantis'];
+    const expected = ['FEAROW', 'FOMANTIS'];
     const actual = capitalizeAndFilter(someArray);
 
     expect(actual).toEqual(expected);
