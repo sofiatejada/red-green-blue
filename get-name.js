be628ff167608f3174974af5df593ecfea542267
Sofia Tejada <sofianais@icloud.com>
2021-07-19 18:30:54 -0700

    get a name returned as expected

--- get-name.js
+++ get-name.js
@@ -1,5 +1,9 @@
-const getName = () => {
-  return 'Storm';
+const getName = (obj) => {
+  // get access to name property
+  // store name property in a variable
+  const egoName = obj.name;
+  // return that variable
+  return egoName;
 };
 
 export default getName;
